ac2c27252a4de6d9d4a1417ecf2b45ad7dc2320c
Osvaldobrito <osvaldooabn@gmail.com>
2025-07-21 20:05:02 -0400

    nova atualização

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/web_project_around_react/",
   plugins: [react()],
   server: { port: 3000 },
 });
